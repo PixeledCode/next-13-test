35a5e892cab64b13db7408a28e62f2578ca1800f
pixeledcode <ashoaib540918@gmail.com>
2023-04-25 18:18:34 +0530

    move css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,45 @@
-import { siteConfig } from "@/config/site"
-import Provider from "@/components/provider"
-import "@/styles/globals.css"
-import { Inter as FontSans } from "next/font/google"
+import { siteConfig } from '@/config/site'
+import Provider from '@/components/provider'
+import '@opub-cdl/ui/dist/assets/styles.css'
+import '@/styles/globals.css'
+import { Inter as FontSans } from 'next/font/google'
 
-const fontSans = FontSans({ subsets: ["latin"] })
+const fontSans = FontSans({ subsets: ['latin'] })
 
 export const metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    "Next.js",
-    "React",
-    "Server Components",
-    "Radix UI",
-    "OPub",
-    "Open Publishing",
-  ],
-  authors: [
-    {
-      name: "CivicDataLab",
-      url: "https://civicdatalab.in/",
-    },
-  ],
-  creator: "CivicDataLab",
+	title: {
+		default: siteConfig.name,
+		template: `%s | ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	keywords: [
+		'Next.js',
+		'React',
+		'Server Components',
+		'Radix UI',
+		'OPub',
+		'Open Publishing',
+	],
+	authors: [
+		{
+			name: 'CivicDataLab',
+			url: 'https://civicdatalab.in/',
+		},
+	],
+	creator: 'CivicDataLab',
 }
 // TODO - Add favicon and other social meta tags
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={fontSans.className}>
-        <Provider>{children}</Provider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={fontSans.className}>
+				<Provider>{children}</Provider>
+			</body>
+		</html>
+	)
 }
